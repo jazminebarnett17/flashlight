fb7a16da0d2fcb9be8f0decdae566ade131b9f2b
Jazmine <jazminebarnett17@augustana.edu>
2021-07-23 10:23:01 -0500

    Initial Commit

---
+++ flashlight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flashlight
+//
+//  Created by Jazmine Barnett on 7/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
